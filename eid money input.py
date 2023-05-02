7091761b98de21b8247fb482277121d69976985c
Nealam Khan <129888512+Nealam123@users.noreply.github.com>
2023-05-02 18:40:13 +0100

    Delete eid money input.py

--- eid money input.py
+++
@@ -1,7 +0,0 @@
-#eid money for people in your house
-
-eid=int(input("could you please let me know how many people there are in the house: "))
-
-money=eid*50
-
-print("There are {} people and I will give a total of {} pounds as a good muslim". format(eid,money))
