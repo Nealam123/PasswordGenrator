f2fd28533ad2bccc48988aede75248c8601a40ee
Nealam Khan <129888512+Nealam123@users.noreply.github.com>
2023-05-02 18:40:54 +0100

    Delete fruit1.py

--- fruit1.py
+++
@@ -1,6 +0,0 @@
-fruit=input("What is your favouiate food?: ")
-print(f"your favouriate food is {fruit}  ")
-
-apples=input("how many apples do you buy per week?: ")
-
-print(f"you buy {int(apples)+5} apples")
